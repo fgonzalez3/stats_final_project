34e6d2415a126b8de9d63c8cf5e366ae8bbf78e8
sw4thinm <119557908+sw4thinm@users.noreply.github.com>
2022-12-03 12:21:36 -0500

    Add files via upload

---
+++ tidydata_initial.R
@@ -0,0 +1,41 @@
+library(dplyr)
+library(doBy)
+library(readr)
+library(ggplot2)
+library(geosphere)
+
+# read in csv containing divvy bike data. This is the raw data file obtained from Kaggle
+
+divy_chicago <- read.csv("data.csv")
+
+# filter to only include ridership data in 2017 and omit any NA
+
+divy_2017 <- divy_chicago |>
+  filter(year == "2017") |>
+  na.omit()
+
+# modify types of variables
+
+divy_2017$month <- as.factor(divy_2017$month)
+divy_2017$day <- as.factor(divy_2017$day)
+divy_2017$hour <- as.factor(divy_2017$hour)
+divy_2017$from_station_name <- as.factor(divy_2017$from_station_name)
+divy_2017$to_station_name <- as.factor(divy_2017$to_station_name)
+divy_2017$gender <- as.factor(divy_2017$gender)
+divy_2017$events <- as.factor(divy_2017$events)
+
+# final version of data used for analyses
+# create column to get distance of bike rides - uses Haversine formula function from geosphere package
+
+start_list <- cbind(c(divy_2017$longitude_start), c(divy_2017$latitude_start))
+end_list <- cbind(c(divy_2017$longitude_end), c(divy_2017$latitude_end))
+
+
+divy_2017 <- divy_2017 |>
+  mutate(tripdistance = distHaversine(end_list, start_list)) |>
+  select(-year, -week, -usertype, -from_station_id, -to_station_id -starttime, -stoptime)
+
+divy_2017 <- select(divy_2017, -year)
+divy_2017 <- select(divy_2017, -starttime, -stoptime)
+  
+write.csv(divy_2017, "divvy_main_2017.csv")
